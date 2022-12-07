51f61feb7b62791cdd5ddbdbcc56133359490669
Anjarul Islam <anjarul.bs23@gmail.com>
2022-12-07 11:15:25 +0600

    testing service worker

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { Teachers } from "./components/Teachers";
 import { Representetives } from "./components/Representetives";
 import { Notifications } from "./components/Notifications";
 import { PrivateRoute } from "./PrivateRoute";
+import { register } from "./serviceWorkerRegistration";
 
 function App() {
   return (
@@ -77,5 +78,7 @@ function App() {
     </Router >
   );
 }
+
+register({ onSuccess: (registration) => console.log('Service registered') });
 
 export default App;
