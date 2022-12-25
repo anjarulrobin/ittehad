76ad72768b950bb5408187ac4f268fb6ddb375d1
Anjarul Islam <anjarul.bs23@gmail.com>
2022-12-25 12:14:18 +0600

    Revert "test fcm"
    This reverts commit 9216212be6aa0cac48c56ea28ed586c1b893c239.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compa
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_FCM_API_KEY,
-    // authDomain: process.env.REACT_APP_FCM_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_FCM_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_FCM_STORAGE_BUCKET,
-    messagingSenderId: "9512057650",
-    // appId: process.env.REACT_APP_FCM_APP_ID,
-    // measurementId: process.env.REACT_APP_FCM_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_FCM_API_KEY,
+    authDomain: process.env.REACT_APP_FCM_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FCM_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FCM_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FCM_SENDER_ID,
+    appId: process.env.REACT_APP_FCM_APP_ID,
+    measurementId: process.env.REACT_APP_FCM_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
