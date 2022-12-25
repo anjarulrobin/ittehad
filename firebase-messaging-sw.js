457aaa866a935d2becd67affc485da406d4a3f96
Anjarul Islam <anjarul.bs23@gmail.com>
2022-12-25 12:12:27 +0600

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,14 +14,15 @@ firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-    console.log('Received background message ', payload);
+// messaging.setBackgroundMessage(function (payload) {
+//     console.log('Received background message ', payload);
 
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-    };
+//     const notificationTitle = payload.notification.title;
+//     const notificationOptions = {
+//         body: payload.notification.body,
+//     };
 
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
+//     self.registration.showNotification(notificationTitle,
+//         notificationOptions);
+// });
+console.log(messaging)
