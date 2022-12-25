ea875d93c5a53c06b374166d4ada63f4e33d8a2c
Anjarul Islam <anjarul.bs23@gmail.com>
2022-12-25 19:00:28 +0600

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
+const firebaseConfig = {
+    apiKey: 'AIzaSyBGX96MisY8l9xoiUL92AV7appSjQwRbFI',
+    authDomain: "ittehad-b28ec.firebaseapp.com",
+    databaseURL: "https://lorem.firebaseio.com",
+    projectId: "ittehad-b28ec",
+    storageBucket: "ittehad-b28ec.appspot.com",
+    messagingSenderId: "9512057650",
+    appId: "1:9512057650:web:62302bec9aee5e3177f52b",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// messaging.onBackgroundMessage(function (payload) {
+//     console.log('Received background message ', payload);
+
+//     const notificationTitle = payload.notification.title;
+//     const notificationOptions = {
+//         body: payload.notification.body,
+//     };
+
+//     self.registration.showNotification(notificationTitle,
+//         notificationOptions);
+// });
