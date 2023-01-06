d666de27f911eb2c6fef32d85e4f0172187b2435
Anjarul Islam <anjarul.bs23@gmail.com>
2023-01-06 21:20:14 +0600

    loader issue fixed

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,11 +2,13 @@ export function Loader({ message = 'অনুগ্রহপূর্বক অ�
     return (
         <div className="flex justify-center items-center h-96">
             <div>
-                <p
-                    className="ml-12 border-2 border-solid border-[#f3f3f3] border-t-2 border-t-[#3498db] rounded-full w-10 h-10 animate-spin"
-                >
-                </p>
-                <p className="w-full mt-3">{message}</p>
+                <div className="flex justify-center w-full">
+                    <p
+                        className="border-2 border-solid border-[#f3f3f3] border-t-2 border-t-[#3498db] rounded-full w-10 h-10 animate-spin"
+                    >
+                    </p>
+                </div>
+                <p className="w-full mt-3 text-center">{message}</p>
             </div>
         </div>
     )
